c1bd1a37ec90bfce074835452920d8bbb6b11355
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2025-01-07 18:03:45 +0200

    updated fe

---
+++ awp_pillkeeper.client/src/stores/eventStore.ts
@@ -0,0 +1,48 @@
+import { create } from "zustand";
+import { RRule } from "rrule";
+
+export type ProcessedEvent = {
+  event_id: number | string;
+  title: string;
+  subtitle?: string;
+  start: Date;
+  end: Date;
+  disabled?: boolean;
+  recurring: RRule;
+  color?: string;
+  textColor?: string;
+  editable?: boolean;
+  deletable?: boolean;
+  draggable?: boolean;
+  allDay?: boolean;
+  agendaAvatar?: React.ReactElement | string;
+  sx?: any; 
+};
+
+interface EventStore {
+  events: ProcessedEvent[];
+  addEvent: (event: ProcessedEvent) => void;
+  updateEvent: (event: ProcessedEvent) => void;
+  deleteEvent: (eventId: number | string) => void;
+}
+
+export const useEventStore = create<EventStore>((set) => ({
+  events: [],
+
+  addEvent: (event) =>
+    set((state) => ({
+      events: [...state.events, event],
+    })),
+
+  updateEvent: (updatedEvent) =>
+    set((state) => ({
+      events: state.events.map((event) =>
+        event.event_id === updatedEvent.event_id ? updatedEvent : event
+      ),
+    })),
+
+  deleteEvent: (eventId) =>
+    set((state) => ({
+      events: state.events.filter((event) => event.event_id !== eventId),
+    })),
+}));
